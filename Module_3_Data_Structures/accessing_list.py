0cea9973a6c4c7b199d72ff49c3e5a599b2d4716
Bboy010 <hkoffianderson@gmail.com>
2025-02-13 19:21:14 -1200

    access to items

---
+++ Module_3_Data_Structures/accessing_list.py
@@ -0,0 +1,13 @@
+species = ["Homo sapiens", "Pan troglodytes", "Gorilla gorilla"]
+
+
+
+# Access the first item
+
+print(f"The specie one is {species[0]}")  # Output: Homo sapiens
+
+
+
+# Access the second item
+
+print(species[1])  # Output: Pan troglodytes
